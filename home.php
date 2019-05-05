b3110aa021417fc3dcf4c55cc1b891faba09a121
@LiburanJogja <arya.dhiratara@gmail.com>
2019-05-05 22:08:34 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Home Page
+ * @author Beans
+ * @link   https://www.getbeans.io
+ * @package Beans\Framework
+ */
+
+beans_remove_action( 'beans_header_partial_template' );
+beans_remove_action( 'beans_footer_partial_template' );
+beans_load_document();
